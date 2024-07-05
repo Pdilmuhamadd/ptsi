1212b4d25c5c18ae4e27123702dd624492526d1f
Hadziq Umar Azka Mirza <122033699+Hadziq224@users.noreply.github.com>
2024-07-05 11:20:07 +0700

    Update footer.blade.php

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,82 +1,39 @@
-
-<footer class="section-footer mt-5 mb-4 border-top">
-    <div class="container pt-5 pb-5">
-        <div class="row justify-content-center">
-           <div class="col-12 ">
-                <div class="row">
+<footer class="footer">
+        <div class="container">
+            <div class="footer-content">
+                <div class="footer-section company-info">
+                    <h5>PT Surveyor Indonesia</h5>
+                    Jl. Jend. Gatot Subroto Kav.56<br>
+                    Jakarta 12950 - Indonesia</p>
+                </div>
+                <div class="footer-section contact-info">
+                    <p><i class="fa fa-phone"></i> 0215265526</p>
+                    <p><i class="fa fa-fax"></i> 0215265525</p>
+                </div>
+                <div class="footer-section logo">
                     <div class="col-12 col-lg-3">
-                        <h5>
-                            FEATURES
-                        </h5>
-                        <ul class="list-unstyled">
-                            <li><a href="#">Reviews</a></li>
-                            <li><a href="#">Community</a></li>
-                            <li><a href="#">Social Media Kit</a></li>
-                            <li><a href="#">Affiliate</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-12 col-lg-3">
-                        <h5>
-                            ACCOUNT
-                        </h5>
-                        <ul class="list-unstyled">
-                            <li><a href="#">Refund</a></li>
-                            <li><a href="#">Security</a></li>
-                            <li><a href="#">Rewards</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-12 col-lg-3">
-                        <h5>
-                            COMPANY
-                        </h5>
-                        <ul class="list-unstyled">
-                            <li><a href="#">Career</a></li>
-                            <li><a href="#">Help Center</a></li>
-                            <li><a href="#">Media</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-12 col-lg-3">
-                        <h5>
-                            GET CONNECTED
-                        </h5>
-                        <ul class="list-unstyled">
-                            <li><a href="https://www.google.com/maps/dir//Graha+Surveyor+Indonesia,+8,+Jl.+Gatot+Subroto+No.Kav.+56,+RT.1%2FRW.4,+Kuningan+Tim.,+Kecamatan+Setiabudi,+Kota+Jakarta+Selatan,+Daerah+Khusus+Ibukota+Jakarta+12950/@-6.2410775,106.7562215,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x2e69f3c2f78e1417:0x6e69dc9146860164!2m2!1d106.8324673!2d-6.2393357?entry=ttu">Jl. Gatot Subroto No.Kav. 56,
-                                 Kota Jakarta Selatan, Indonesia</a></li>
-                            <li><a href="#">0215265526</a></li>
-                            <li><a href="#">surveyorindonesia@ptsi.co.id</a></li>
-
-                        </ul>
-                    </div>
-                    <div class="col-12 col-lg-3">
-                    <a href="{{url('https://bumn.go.id/')}}" class="bumn-brand">
-                        <h5>
-                        <img src="{{url('frontend/images/bumn.png')}}" alt="bumn"/>
-                        </h5>
-                        <button class="navbar-toggler navbar-toggler-right"
-                         type="button"
-                        data-toggle="collapse"
-                        data-targer="#navb"
-                        >
-                        </button>
+                        <a href="{{url('https://bumn.go.id/')}}" class="bumn-brand">
+                            <h5>
+                            <img src="{{url('frontend/images/bumn.png')}}" alt="bumn"/>
+                            </h5>
+                            <button class="navbar-toggler navbar-toggler-right"
+                             type="button"
+                            data-toggle="collapse"
+                            data-targer="#navb"
+                            >
+                            </button>
+                        </a>
                     </div>
                 </div>
-           </div>
+            </div>
+            <div class="footer-bottom">
+                <p>&copy; 2024 PT Surveyor Indonesia. All Rights Reserved.</p>
+                <div class="social-media">
+                    <a href="https://www.facebook.com/surveyor.id" Target="_blank" rel="noopener noreferrer"><i class="fa fa-facebook"></i></a>
+                    <a href="https://www.linkedin.com/company/suveryor-id/" Target="_blank" rel="noopener noreferrer"><i class="fa fa-linkedin"></i></a>
+                    <a href="https://www.youtube.com/@surveyor_ID" Target="_blank" rel="noopener noreferrer"><i class="fa fa-youtube"></i></a>
+                    <a href="https://www.instagram.com/surveyor.id" Target="_blank" rel="noopener noreferrer"><i class="fa fa-instagram"></i></a>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="contacts">
-        <div class = "justify-content-center align-items-center pt-4">
-        <ul class="contacts">
-            <li><a href="https://www.instagram.com/surveyor.id">Instagram</a></li>
-            <li><a href="https://www.youtube.com/@surveyor_ID">Youtube</a></li>
-            <li><a href="https://www.linkedin.com/company/suveryor-id">LinkedIn</a></li>
-            <li><a href="https://www.facebook.com/surveyor.id">Facebook</a></li>
-        </ul>
-    </div>
-    <div class="container-fluid">
-    <div class="justify-content-center align-items-center pt-4">
-        <div class="col-auto text-blue-500 font-weight-light">
-            <a> 2024 Copyright PT. Surveyor Indonesia • All rights reserved • Made in Jakarta
-        </div>
-    </div>
-</div>
-</footer>
+    </footer>
