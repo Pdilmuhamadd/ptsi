6a2f1538f64ef2f7a222d10a74dd78bc2e702504
Fadhil Muhamad Pratama <91975626+Pdilmuhamadd@users.noreply.github.com>
2024-07-08 14:03:00 +0700

    Update detail.blade.php

--- resources/views/pages/admin/detail.blade.php
+++ resources/views/pages/admin/detail.blade.php
@@ -25,12 +25,12 @@
                 <div class="col-lg-8 pl-lg-0">
                     <div class="card card-details">
                     <h1>
-                        {{ $item->title}}
+                        {{-- {{ $item->title}} --}}
                     </h1>
                     <p>
-                        {{ $item->location}}
+                        {{-- {{ $item->location}} --}}
                     </p>
-                    @if ($item->galleries->count())
+                    {{-- @if ($item->galleries->count())
                     <div class="gallery">
                         <div class="xzoom-container">
                             <img src="{{Storage::url($item->galleries->first()->image)}}"
@@ -49,11 +49,11 @@
                             @endforeach
                         </div>
                     </div>
-                    @endif
+                    @endif --}}
                     <h2>
                         Tentang Proyek
                     </h2>
-                    {!! $item->about !!}
+                    {{-- {!! $item->about !!} --}}
                 </div>
                 <div class="features row">
                     <div class="col-md-4">
@@ -66,7 +66,7 @@
                                     Unit
                                 </h3>
                                 <p>
-                                    {{$item->featured_event}}
+                                    {{-- {{$item->featured_event}} --}}
                                 </p>
                             </div>
                         </div>
@@ -81,7 +81,7 @@
                                     PIC
                                 </h3>
                                 <p>
-                                    {{$item->language}}
+                                    {{-- {{$item->language}} --}}
                                 </p>
                             </div>
                             </div>
@@ -96,7 +96,7 @@
                                     Status
                                 </h3>
                                 <p>
-                                    {{$item->foods}}
+                                    {{-- {{$item->foods}} --}}
                                 </p>
                             </div>
                         </div>
@@ -106,22 +106,6 @@
                 <div class="col-lg-4">
                     <div class="card card-details card-right">
                         <h2>
-                            Member are going
-                        </h2>
-                        <div class="members my-2">
-                            <img src="{{url('frontend/images/member-1.png')}}"
-                            class="member-image mr-1" />
-                            <img src="{{url('frontend/images/member-2.png')}}"
-                            class="member-image mr-1" />
-                            <img src="{{url('frontend/images/member-3.png')}}"
-                            class="member-image mr-1" />
-                            <img src="{{url('frontend/images/member-4.png')}}"
-                            class="member-image mr-1" />
-                            <img src="{{url('frontend/images/member-5.png')}}"
-                            class="member-image mr-1" />
-                        </div>
-                        <hr />
-                        <h2>
                             Informasi Proyek
                         </h2>
                         <table class="trip-informations">
@@ -130,7 +114,7 @@
                                     Tanggal Mulai
                                 </th>
                                 <td width="50%" class="text-right">
-                                    {{\Carbon\Carbon::create($item->date_of_departure)->format('F n, Y')}}
+                                    {{-- {{\Carbon\Carbon::create($item->date_of_departure)->format('F n, Y')}} --}}
                                 </td>
                             </tr>
                             <tr>
@@ -138,7 +122,7 @@
                                     Target Selesai
                                 </th>
                                 <td width="50%" class="text-right">
-                                    {{$item->duration}}
+                                    {{-- {{$item->duration}} --}}
                                 </td>
                             </tr>
                             <tr>
@@ -146,24 +130,10 @@
                                     Anggaran
                                 </th>
                                 <td width="50%" class="text-right">
-                                    Rp.{{$item->price}},00
+                                    {{-- Rp.{{$item->price}},00 --}}
                                 </td>
                             </tr>
                         </table>
-                    </div>
-                    <div class="join-container">
-                        @auth
-                        	<form action="{{route('checkout-process', $item->id)}}" method="POST">
-                                @csrf
-                                <button class="btn btn-block btn-join-now mt-3 py-2" type="submit">
-                                    Join Now
-                                </button>
-                        @endauth
-                        @guest
-                            <a href="{{url('login')}}" class="btn btn-block btn-join-now mt-3 py-2">
-                                Login or Register to Join
-                            </a>
-                        @endguest
                     </div>
                 </div>
             </div>
