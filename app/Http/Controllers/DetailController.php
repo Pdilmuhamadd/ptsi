4786284866e3dfe6fd51af7937ee8b13660d19e1
Fadhil Muhamad Pratama <91975626+Pdilmuhamadd@users.noreply.github.com>
2024-07-10 13:43:59 +0700

    Update DetailController.php

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -2,18 +2,63 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\TravelPackage;
+use App\Models\paket_proyek;
 use Illuminate\Http\Request;
 
 class DetailController extends Controller
 {
-    public function index(Request $request, $slug)
+    public function index()
     {
-        $item = TravelPackage::with(['galleries'])
-                    ->where('slug', $slug)
-                    ->firstOrFail();
-        return view('pages.admin.detail',[
-            'item' => $item
+        return paket_proyek::all();
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nama_proyek' => 'required|string|max:255',
+            'deskripsi_proyek' => 'nullable|string',
+            'tanggal_mulai' => 'required|date',
+            'target_selesai' => 'required|date',
+            'estimasi_biaya' => 'required|numeric',
+            'pic' => 'required|string|max:255',
+            'mp' => 'required|string|max:255',
+            'sumber_daya' => 'required|integer',
         ]);
+
+        $paket_proyek = paket_proyek::create($validatedData);
+
+        return response()->json($paket_proyek, 201);
+    }
+
+    public function show($id)
+    {
+        $item = paket_proyek::findOrFail($id);
+        return view('pages.admin.detail', compact('item'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'nama_proyek' => 'sometimes|required|string|max:255',
+            'deskripsi_proyek' => 'sometimes|nullable|string',
+            'tanggal_mulai' => 'sometimes|required|date',
+            'target_selesai' => 'sometimes|required|date',
+            'estimasi_biaya' => 'sometimes|required|numeric',
+            'pic' => 'sometimes|required|string|max:255',
+            'mp' => 'sometimes|required|string|max:255',
+            'sumber_daya' => 'sometimes|required|integer',
+        ]);
+
+        $paket_proyek = paket_proyek::findOrFail($id);
+        $paket_proyek->update($validatedData);
+
+        return response()->json($paket_proyek, 200);
+    }
+
+    public function destroy($id)
+    {
+        paket_proyek::destroy($id);
+
+        return response()->json(null, 204);
     }
 }
